109fa8c50c03f8ea05a20455fd00902fa8d6fbf2
Baby Yoda <patrihinaangelina@gmail.com>
2022-10-02 01:33:17 +0300

    my first commit

---
+++ inten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#coding: latin-1
+import os, sys
+import random
+
+def random_pass():
+    login = input()
+    pswrd = ''
+    for i in range(16):
+        pswrd = pswrd + random.choice(list('!,.@#%^&*()1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+    return "Привет, " + str(login) + "! password of yours: " + str(pswrd)
+
+
+if __name__ == "__main__":
+    password = random_pass()
+    print(password)
